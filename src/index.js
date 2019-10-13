5da6d0665807424379cb2669ae1337f3b0161194
DenisMezhuev <alexmezhuev@yandex.by>
2019-10-13 14:22:30 +0300

    feat: function number, number2, Counter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let arr = expression.replace(/\*/g, " ").split(' ');
+  console.log(arr);
+  let result = 1;
+  
+   let numberTwos = 0;
+   let numberFives = 0; 
+  
+  for(let i = 0; i < arr.length; i++){
+     
+   arr[i].includes("!!") ?  number2(arr[i]) :  number(arr[i]);
+    
+  }
+  
+ 
+ 
+  
+ function Counter (number) {
+     for ( ; ; ) {
+          if (number % 5 == 0) {
+              numberFives++;
+              number /= 5;
+          } else if (number % 2 == 0) {
+              numberTwos++;
+              number /= 2;;
+          } else break;
+     }
+  }
+  
+    function number2(num){
+   let result = 1;
+     let i;
+    
+     for(i = parseInt(num, 10); i > 1; i -= 2){
+           Counter(i);
+     }     
+ }
+ function number(num){
+   let result = 1;
+     let i;
+     for(i = parseInt(num, 10); i > 1; i --){
+           Counter(i);
+     } 
+ }
+  
+return Math.min(numberTwos, numberFives);
+    
 }
